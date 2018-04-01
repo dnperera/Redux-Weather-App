2752a25898bfcbacff5ba289d069893879431de1
Dayan Perera <dayannp@gmail.com>
2018-04-01 06:52:29 -0700

    edit redux action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 const API_KEY = '9dbfdf9304bfd934de48809a28427e5a';
-const ROOT_URL = process.env.WEATHER_API + API_KEY;
-console.log('ROOT_URL-->' + ROOT_URL);
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
+	console.log(process.env.WEATHER_API);
 	const url = `${ROOT_URL}&q=${city},us`;
 	const request = axios.get(url);
 	return {
