536539fd024676fcd0c78b854f7fc980c4c75d64
Dayan Perera <dayannp@gmail.com>
2018-03-31 23:33:09 -0700

    edit openweb url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 const API_KEY = '9dbfdf9304bfd934de48809a28427e5a';
 const ROOT_URL = process.env.WEATHER_API + API_KEY;
-
+console.log(ROOT_URL);
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},us`;
-	console.log(url);
 	const request = axios.get(url);
 	return {
 		type: FETCH_WEATHER,
